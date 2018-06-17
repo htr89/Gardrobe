d149a2c97b095cb61057bd1d3df99ab4cfcbce67
Hermann-PC <Hermann-PC@192.168.0.52>
2018-06-17 16:45:46 +0200

    check

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow} = electron;
+
+
+let mainWindow;
+
+
+app.on('ready', function(){
+	main
+});
