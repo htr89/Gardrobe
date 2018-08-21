b230520b563ae684059a3ad043fe38bb3478a834
Hermann-PC <Hermann-PC@192.168.0.52>
2018-08-21 23:17:12 +0200

    Electron

--- js/main.js
+++ js/main.js
@@ -1,18 +1,52 @@
-const electron = require('electron');
-const url = require('url');
-const path = require('path');
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+const url = require('url')
 
-const {app, BrowserWindow} = electron;
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
 
+function createWindow() {
+    // Erstellen des Browser-Fensters.
+    win = new BrowserWindow({ width: 1080, height: 720 })
 
-let mainWindow;
+    // und Laden der index.html der App.
+    win.loadFile('html/gardrobe.html')
 
+    // Open the DevTools.
+    win.webContents.openDevTools()
 
-app.on('ready', function(){
-	mainWindow = new BrowserWindow({});
-    mainWindow.loadURl(url.format({
-        pathname: path.join(__dirname, 'gardrobe.html'),
-        protocol: 'file:',
-        slashes: true
-    })); 
-});
+    // Ausgegeben, wenn das Fenster geschlossen wird.
+    win.on('closed', () => {
+        // Dereferenzieren des Fensterobjekts, normalerweise würden Sie Fenster
+        // in einem Array speichern, falls Ihre App mehrere Fenster unterstützt. 
+        // Das ist der Zeitpunkt, an dem Sie das zugehörige Element löschen sollten.
+        win = null
+    })
+}
+
+// Diese Methode wird aufgerufen, wenn Electron mit der
+// Initialisierung fertig ist und Browserfenster erschaffen kann.
+// Einige APIs können nur nach dem Auftreten dieses Events genutzt werden.
+app.on('ready', createWindow)
+
+// Verlassen, wenn alle Fenster geschlossen sind.
+app.on('window-all-closed', () => {
+    // Unter macOS ist es üblich für Apps und ihre Menu Bar
+    // aktiv zu bleiben bis der Nutzer explizit mit Cmd + Q die App beendet.
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    // Unter macOS ist es üblich ein neues Fenster der App zu erstellen, wenn
+    // das Dock Icon angeklickt wird und keine anderen Fenster offen sind.
+    if (win === null) {
+        createWindow()
+    }
+})
+
+// In dieser Datei können Sie den Rest des App-spezifischen 
+// Hauptprozess-Codes einbinden. Sie können den Code auch 
+// auf mehrere Dateien aufteilen und diese hier einbinden.
